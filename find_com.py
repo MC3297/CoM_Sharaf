3ba6f8e33871ea220f3d6c769e9884969f3ab889
MC3297 <root@DESKTOP-KU8SBFQ.localdomain>
2024-11-22 02:23:58 -0500

    prettify code

--- find_com.py
+++ find_com.py
@@ -1,91 +1,51 @@
-from svgpathtools import svg2paths, Path, Line, QuadraticBezier, CubicBezier
+from svgpathtools import svg2paths, Path
 import numpy as np
 
 def real(z):
-    try:
-        return np.poly1d(z.coeffs.real)
-    except AttributeError:
-        return z.real
-
+    try: return np.poly1d(z.coeffs.real)
+    except AttributeError: return z.real
 def imag(z):
-    try:
-        return np.poly1d(z.coeffs.imag)
-    except AttributeError:
-        return z.imag
-
-def weighted_area(self):
-    def reg_area(path):#for testing
-        area_enclosed = 0
-        for seg in path:
-            x = real(seg.poly())
-            y = imag(seg.poly())
-            dx = real(seg.poly()).deriv()
-            dy = imag(seg.poly()).deriv()
-            integrand = x*dy
-            integral = integrand.integ()
-            area_enclosed += integral(1) - integral(0)
-        return area_enclosed
-    
+    try: return np.poly1d(z.coeffs.imag)
+    except AttributeError: return z.imag
+def get_coms(self):
     def x_com_sum(path):
-        area_enclosed = 0
+        region = 0
         for seg in path:
             x = real(seg.poly())
             y = imag(seg.poly())
             dx = real(seg.poly()).deriv()
-            dy = imag(seg.poly()).deriv()
             integrand = x*y*dx
             integral = integrand.integ()
-            area_enclosed += integral(1) - integral(0)
-        return area_enclosed
-    
+            region += integral(1) - integral(0)
+        return region
     def y_com_sum(path):
-        area_enclosed = 0
+        region = 0
         for seg in path:
             x = real(seg.poly())
             y = imag(seg.poly())
-            dx = real(seg.poly()).deriv()
             dy = imag(seg.poly()).deriv()
             integrand = y*x*dy
             integral = integrand.integ()
-            area_enclosed += integral(1) - integral(0)
-        return area_enclosed
-
-    assert self.isclosed()
-
-    path_approx = []
-    for seg in self:
-        path_approx.append(seg)
-    #return (abs(x_com_sum(Path(*path_approx))), abs(y_com_sum(Path(*path_approx))), abs(reg_area(Path(*path_approx))))
-    return (abs(x_com_sum(Path(*path_approx))), abs(y_com_sum(Path(*path_approx))))
+            region += integral(1) - integral(0)
+        return region
+    path = [seg for seg in self]
+    return (abs(x_com_sum(Path(*path))), abs(y_com_sum(Path(*path))))
 
 paths, attributes = svg2paths("edited.svg")
-
-cnt = 0
-areas = []
-xcoms = []
-ycoms = []
+areas, xcomsum, ycomsum = [], [], []
 region_id = 0
 for path in paths:
-    A = Path.area(path)
+    A = Path.area(path)#equivalent to using x*dy in above functions
     areas.append(A)
-    K = weighted_area(path)
-    
-    #xcom and ycom of this region
-    #print(K[0]/A, K[1]/A)
-    print(f"Region {region_id:02d} | Segments: {len(path):02d} | Area: {Path.area(path):13f}")
-    
-    for segment in path:
-        print(segment)
-
-    xcoms.append(K[0])
-    ycoms.append(K[1])
-    #print("Area:", A, "Segments:", len(path))
-    cnt += 1
+    K = get_coms(path)
+    xcomsum.append(K[0])
+    ycomsum.append(K[1])
     region_id += 1
-#<circle cx="484" cy="390" r="10" fill="red"/>
 
 total_area = areas[0] - sum(areas[1:])
-xsum = xcoms[0] - sum(xcoms[1:])
-ysum = ycoms[0] - sum(ycoms[1:])
+print("total area:", total_area)
+xcom = (xcomsum[0] - sum(xcomsum[1:])) / total_area
+ycom = (ycomsum[0] - sum(ycomsum[1:])) / total_area
 
-print("xcom and ycom are:", xsum/total_area, ysum/total_area)
+#shift coordinate system so origin lies on figure by subtracting 200
+print("xcom and ycom are:", xcom-200, ycom-200)
